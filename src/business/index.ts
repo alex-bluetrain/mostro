5e135d59cd4967888d2dbd2ea6529d849aea8432
Alex Verstraeten <averstraeten@gmail.com>
2026-07-22 12:20:08 -0300

    feat: create business module public API

---
+++ src/business/index.ts
@@ -0,0 +1,9 @@
+// Models (for type imports)
+export { type IUser } from './models/user.model';
+export { type ISubscriber } from './models/subscriber.model';
+export { type IInvite } from './models/invite.model';
+
+// Repositories
+export { userRepository } from './repositories';
+export { subscriberRepository } from './repositories';
+export { inviteRepository } from './repositories';
